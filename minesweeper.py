af934a2e4761d187990f06602f1b1f46e4aa78fd
Sad <nguyentrungson217@gmail.com>
2023-06-05 04:56:07 +0700

    Set Up BrainStorm For Minesweeper Game On Console

---
+++ minesweeper.py
@@ -0,0 +1,29 @@
+# Create a Minesweeper Game using Python Terminal
+# Author: Sad Nguyen
+# -----------------------------------------------------------------------------------------
+# Welcome to my Minesweeper Game Project
+# ----------------------------------------------------------------------------------------
+
+#! Think of an idea
+# Who do you want to build something for? Yourself? Your friends? Your family? Your co-workers?
+# I want to build it for myself and my friends to play
+
+# What are your/their hobbies?
+# To create more fun games
+
+# Do you want to build something more fun or something more useful?
+# Yes, I will definitely try my best to develop such a thing that's bring joy to everyone
+
+#! Project Brainstorming
+# What does your program do?
+# Challenge the player to allocate all the bombs on the map and win
+
+#* How will it work in a terminal?
+#? Step 1: Print out the board
+#? Step 2: let the player press the index
+#? Step 3: if boom ? kaboom : continue
+#? Step 4: if out_of_boom ? win : continue
+#? Step 5: Print out the Victory Screen
+
+# Is it one player or two players?
+# One Player Game
